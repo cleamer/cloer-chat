7383c8fdfd0c6197453142f62e4d1001e18701b5
cloer <chltjrrb97@gmail.com>
2022-04-03 12:10:29 -0400

    FIX: SignWarningMessage -> WarningMessage

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Nav from './Nav';
 import RoomList from './RoomList';
 import HomeInfo from './HomeInfo';
-import SignWarningMessage from './SignWarningMessage';
+import WarningMessage from './WarningMessage';
 import MessageList from './MessageList';
 import NavHeader from './NavHeader';
 import NavHeaderBack from './NavHeaderBack';
@@ -12,4 +12,4 @@ import NavHeaderMenu from './NavHeaderMenu';
 import RoomInput from './RoomInput';
 import NewRoom from './NewRoom';
 
-export { RequireSigIn, RequireSignOut, Header, Nav, RoomList, HomeInfo, SignWarningMessage, MessageList, NavHeader, RoomInput, NewRoom, NavHeaderBack, NavHeaderMenu };
+export { RequireSigIn, RequireSignOut, Header, Nav, RoomList, HomeInfo, WarningMessage, MessageList, NavHeader, RoomInput, NewRoom, NavHeaderBack, NavHeaderMenu };
